268420de0e3550a5116a5664b12dccd2148e1fe3
sorz <orz@sorz.org>
2017-02-19 21:17:09 +0800

    Initial commit

---
+++ app/src/main/java/org/sorz/lab/sms2smtp/SmsReceiver.java
@@ -0,0 +1,64 @@
+package org.sorz.lab.sms2smtp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.telephony.SmsMessage;
+
+import org.apache.commons.lang3.text.StrSubstitutor;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A broadcast receiver on SMS_RECEIVED_ACTION.
+ * Build Email subject & body then hand to SmtpService.
+ *
+ * Created by xierch on 2017/2/18.
+ */
+
+public class SmsReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        if (!preferences.getBoolean(context.getString(R.string.pref_enable_smtp_forwarding_key), false))
+            return;
+        SmsMessage messages[] = getMessagesFromIntent(intent);
+        for (SmsMessage msg : messages) {
+            final Date date = new Date(msg.getTimestampMillis());
+            final DateFormat dateFormat = android.text.format.DateFormat.getLongDateFormat(context);
+            final DateFormat timeFormat = android.text.format.DateFormat.getTimeFormat(context);
+
+            final Map<String, String> values = new HashMap<>();
+            values.put("from", msg.getDisplayOriginatingAddress());
+            values.put("message", msg.getDisplayMessageBody());
+            values.put("subject", msg.getPseudoSubject());
+            values.put("date", dateFormat.format(date));
+            values.put("time", timeFormat.format(date));
+            final StrSubstitutor sub = new StrSubstitutor(values);
+
+            final String subject = sub.replace(context.getString(R.string.email_subject_template));
+            final String content = sub.replace(context.getString(R.string.email_content_template));
+            SmtpService.startSendEmail(context, subject, content, date.getTime());
+
+            System.out.println(subject);
+            System.out.println(content);
+        }
+    }
+
+    private static SmsMessage[] getMessagesFromIntent(Intent intent) {
+        Object[] messages;
+        messages = (Object[]) intent.getSerializableExtra("pdus");
+        int pduCount = messages.length;
+        SmsMessage[] msgs = new SmsMessage[pduCount];
+
+        for (int i = 0; i < pduCount; i++)
+            msgs[i] = SmsMessage.createFromPdu((byte[]) messages[i]);
+        return msgs;
+    }
+}
---
+++ app/src/main/java/org/sorz/lab/sms2smtp/SmtpService.java
@@ -0,0 +1,109 @@
+package org.sorz.lab.sms2smtp;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class SmtpService extends IntentService {
+    // TODO: Rename actions, choose action names that describe tasks that this
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_SEND_EMAIL = "org.sorz.lab.sms2smtp.action.SEND_EMAIL";
+
+    private static final String EXTRA_SUBJECT = "org.sorz.lab.sms2smtp.extra.SUBJECT";
+    private static final String EXTRA_CONTENT = "org.sorz.lab.sms2smtp.extra.CONTENT";
+    private static final String EXTRA_TIMESTAMP = "org.sorz.lab.sms2smtp.extra.TIMESTAMP";
+
+
+    public SmtpService() {
+        super("SmtpService");
+    }
+
+    /**
+     * Starts this service to send SMS via SMTP.
+     */
+    public static void startSendEmail(Context context, String subject, String content, long timestamp) {
+        Intent intent = new Intent(context, SmtpService.class);
+        intent.setAction(ACTION_SEND_EMAIL);
+        intent.putExtra(EXTRA_SUBJECT, subject);
+        intent.putExtra(EXTRA_CONTENT, content);
+        intent.putExtra(EXTRA_TIMESTAMP, timestamp);
+        context.startService(intent);
+    }
+
+    public static void startSendEmail(Context context, String subject, String content) {
+        startSendEmail(context, subject, content, new Date().getTime());
+
+    }
+
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_SEND_EMAIL.equals(action)) {
+                final String subject = intent.getStringExtra(EXTRA_SUBJECT);
+                final String content = intent.getStringExtra(EXTRA_CONTENT);
+                final long timestamp = intent.getLongExtra(EXTRA_TIMESTAMP, new Date().getTime());
+                handleSendEmail(subject, content, timestamp);
+            }
+        }
+    }
+
+    /**
+     * Send email in the provided background thread with the provided
+     * subject and content.
+     */
+    private void handleSendEmail(String subject, String content, long timestamp) {
+        final Context context = getBaseContext();
+        final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        final String from = preferences.getString(context.getString(R.string.pref_email_from_key), "");
+        final String recipient =
+                preferences.getString(context.getString(R.string.pref_email_recipient_key), "");
+        final String host = preferences.getString(context.getString(R.string.pref_smtp_server_key), "");
+        int port = 25;
+        try {
+            port = Integer.parseInt(
+                    preferences.getString(context.getString(R.string.pref_smtp_port_key), "25"));
+        } catch (NumberFormatException e) {
+        // TODO: tell user.
+        }
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
+        Session session = Session.getInstance(props, null);
+        MimeMessage msg = new MimeMessage(session);
+        try {
+            msg.setFrom(new InternetAddress(from));
+            msg.setRecipients(Message.RecipientType.TO, recipient);
+            msg.setSubject(subject);
+            msg.setSentDate(new Date(timestamp));
+            msg.setText(content);
+            Transport.send(msg);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
