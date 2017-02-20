d7e09deb54630fa0c634717b876941bd94a075d2
sorz <orz@sorz.org>
2017-02-21 05:08:12 +0800

    Concatenate multipart SMS

--- app/src/main/java/org/sorz/lab/sms2smtp/SmsReceiver.java
+++ app/src/main/java/org/sorz/lab/sms2smtp/SmsReceiver.java
@@ -11,6 +11,7 @@ import android.util.Log;
 import org.apache.commons.lang3.text.StrSubstitutor;
 
 import java.text.DateFormat;
+import java.util.Collection;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +26,22 @@ import java.util.Map;
 public class SmsReceiver extends BroadcastReceiver {
     private static final String TAG = "SmsReceiver";
 
+    private static class Message {
+        String sender;
+        String body;
+        long timestamp;
+
+        Message(SmsMessage sms) {
+            sender = sms.getDisplayOriginatingAddress();
+            body = sms.getDisplayMessageBody();
+            timestamp = sms.getTimestampMillis();
+        }
+
+        void concat(SmsMessage sms) {
+            body += sms.getDisplayMessageBody();
+        }
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
         final SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
@@ -32,17 +49,17 @@ public class SmsReceiver extends BroadcastReceiver {
             Log.d(TAG, "New SMS ignored according to the global switch.");
             return;
         }
-        SmsMessage messages[] = getMessagesFromIntent(intent);
-        Log.i(TAG, messages.length + " SMS received.");
-        for (SmsMessage msg : messages) {
-            final Date date = new Date(msg.getTimestampMillis());
+        SmsMessage smses[] = getMessagesFromIntent(intent);
+        Collection<Message> messages = retrieveSenderMessage(smses);
+        Log.i(TAG, messages.size() + " SMS received, from " + smses.length + " parts");
+        for (Message msg : messages ) {
+            final Date date = new Date(msg.timestamp);
             final DateFormat dateFormat = android.text.format.DateFormat.getLongDateFormat(context);
             final DateFormat timeFormat = android.text.format.DateFormat.getTimeFormat(context);
 
             final Map<String, String> values = new HashMap<>();
-            values.put("from", msg.getDisplayOriginatingAddress());
-            values.put("message", msg.getDisplayMessageBody());
-            values.put("subject", msg.getPseudoSubject());
+            values.put("from", msg.sender);
+            values.put("message", msg.body);
             values.put("date", dateFormat.format(date));
             values.put("time", timeFormat.format(date));
             final StrSubstitutor sub = new StrSubstitutor(values);
@@ -52,6 +69,24 @@ public class SmsReceiver extends BroadcastReceiver {
             Log.d(TAG, "Request sending email, subject: " + subject);
             SmtpService.startSendEmail(context, subject, content, date.getTime());
         }
+    }
+
+    /**
+     * Merge messages with same sender (if any) to one message.
+     * @param messages A array of SmsMessage parsing from a single PDUS.
+     * @return A set of joined messages.
+     */
+    private static Collection<Message> retrieveSenderMessage(SmsMessage[] messages) {
+        HashMap<String, Message> senderBody = new HashMap<>(messages.length);
+        for (SmsMessage sms : messages) {
+            String sender = sms.getOriginatingAddress();
+            if (!senderBody.containsKey(sender)) {
+                senderBody.put(sender, new Message(sms));
+            } else {
+                senderBody.get(sender).concat(sms);
+            }
+        }
+        return senderBody.values();
     }
 
     private static SmsMessage[] getMessagesFromIntent(Intent intent) {
